675be7531684440b4bd794a2fd56a3760dc8f7a7
Willy19921992 <guuillekarateka@gmail.com>
2024-05-09 13:09:47 +0200

    imgui cambiar texto por dropdowns

--- src/GPO_imgui_aux.cpp
+++ src/GPO_imgui_aux.cpp
@@ -34,9 +34,10 @@ void imgui_newframe(void) {
 bool imgui_renderShaderSelect(int* nProg) {
 	static int sel = 0;
 
-	ImGui::Text("Select shader");
-	ImGui::RadioButton("Phong", &sel, 0); ImGui::SameLine();
-	ImGui::RadioButton("Blinn", &sel, 1);
+	if (ImGui::CollapsingHeader("Select shader", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::RadioButton("Phong", &sel, 0); ImGui::SameLine();
+		ImGui::RadioButton("Blinn", &sel, 1);
+	}
 
 	if (sel != *nProg) {
 		*nProg = sel;
@@ -48,9 +49,10 @@ bool imgui_renderShaderSelect(int* nProg) {
 bool imgui_renderSceneSelect(int* nScene) {
 	static int sel = 0;
 
-	ImGui::Text("Select scene");
-	ImGui::RadioButton("Isla", &sel, 0); ImGui::SameLine();
-	ImGui::RadioButton("Torre", &sel, 1);
+	if (ImGui::CollapsingHeader("Select scene", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::RadioButton("Isla", &sel, 0); ImGui::SameLine();
+		ImGui::RadioButton("Torre", &sel, 1);
+	}
 
 	if (sel != *nScene) {
 		*nScene = sel;
@@ -62,10 +64,11 @@ bool imgui_renderSceneSelect(int* nScene) {
 bool imgui_renderCameraPos(float* d, float* az, float* el) {
 	bool res = false;
 
-	ImGui::Text("Camera position");
-	res |= ImGui::SliderFloat("Distance##camara", d, 2.0f, 16.0f, "%.3f", ImGuiSliderFlags_NoRoundToFormat);
-	res |= ImGui::SliderAngle("Azimuth##camara", az, 0.0f, 360.0f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
-	res |= ImGui::SliderAngle("Elevation##camara", el, -89.999f, 89.999f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+	if (ImGui::CollapsingHeader("Camera position", ImGuiTreeNodeFlags_DefaultOpen)) {
+		res |= ImGui::SliderFloat("Distance##camara", d, 2.0f, 16.0f, "%.3f", ImGuiSliderFlags_NoRoundToFormat);
+		res |= ImGui::SliderAngle("Azimuth##camara", az, 0.0f, 360.0f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+		res |= ImGui::SliderAngle("Elevation##camara", el, -89.999f, 89.999f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+	}
 
 	return res;
 }
@@ -73,25 +76,28 @@ bool imgui_renderCameraPos(float* d, float* az, float* el) {
 bool imgui_renderLightVec(float* az, float* el) {
 	bool res = false;
 
-	ImGui::Text("Global light vector");
-	res |= ImGui::SliderAngle("Azimuth##luzglobal", az, 0.0f, 360.0f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
-	res |= ImGui::SliderAngle("Elevation##luzglobal", el, -89.999f, 89.999f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+	if (ImGui::CollapsingHeader("Global light vector", ImGuiTreeNodeFlags_DefaultOpen)) {
+		res |= ImGui::SliderAngle("Azimuth##luzglobal", az, 0.0f, 360.0f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+		res |= ImGui::SliderAngle("Elevation##luzglobal", el, -89.999f, 89.999f, "%.1f deg", ImGuiSliderFlags_NoRoundToFormat);
+	}
 
 	return res;
 }
 
 void imgui_renderLightColor(vec3* color) {
-	ImGui::Text("Lighting color");
-	ImGui::ColorPicker3("##color", &color->r, //Esto es una autentica guarrada que puede explotar en cualquier momento
-		ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_DisplayRGB);
+	if (ImGui::CollapsingHeader("Lighting color", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::ColorPicker3("##color", &color->r, //Esto es una autentica guarrada que puede explotar en cualquier momento
+							ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_DisplayRGB);
+	}
 }
 
 void imgui_renderCoefficients(vec4* coeficientes) {
-	ImGui::Text("Lighting coefficients");
-	ImGui::SliderFloat("Ambient##coeficientes", &(coeficientes->x), 0.0f, 1.0f, "%.2f");
-	ImGui::SliderFloat("Diffuse##coeficientes", &(coeficientes->y), 0.0f, 1.0f, "%.2f");
-	ImGui::SliderFloat("Specular##coeficientes", &(coeficientes->z), 0.0f, 1.0f, "%.2f");
-	ImGui::SliderFloat("Shininess##coeficientes", &(coeficientes->w), 1.0f, 100.0f, "%.0f");
+	if (ImGui::CollapsingHeader("Lighting coefficients", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::SliderFloat("Ambient##coeficientes", &(coeficientes->x), 0.0f, 1.0f, "%.2f");
+		ImGui::SliderFloat("Diffuse##coeficientes", &(coeficientes->y), 0.0f, 1.0f, "%.2f");
+		ImGui::SliderFloat("Specular##coeficientes", &(coeficientes->z), 0.0f, 1.0f, "%.2f");
+		ImGui::SliderFloat("Shininess##coeficientes", &(coeficientes->w), 1.0f, 100.0f, "%.0f");
+	}
 }
 
 void imgui_renderframe(void) {
